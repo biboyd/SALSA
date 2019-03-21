41de8237632c8a649a3ec995bcf46822510dfd62
boydbre1 <boydbre1@msu.edu>
2019-03-20 21:33:32 -0400

    added center to slicing

--- plotting_ray/slice_and_ray.py
+++ plotting_ray/slice_and_ray.py
@@ -1,4 +1,4 @@
-import yt
+erimport yt
 import trident as tri
 import numpy as np
 
@@ -9,12 +9,13 @@ filename=home+"data/DD0076/DD0076"
 ds = yt.load(filename)
 
 ray_start= np.array([0, 0, 0], dtype=float)
-ray_end = np.array([1, 1, 1], dtype=float)
+ray_end = np.array([0.5, 1, 1], dtype=float)
 
 line_list = ['H', 'Si', 'Mg II', 'C II 1335']
 
 #calc norm vec and center
 center = (ray_end + ray_start)/2
+center[2]=0.5
 
 #find ray and normalize it
 ray = ray_end - ray_start
@@ -23,8 +24,8 @@ ray = ray/np.linalg.norm(ray)
 #find ray perpendicular with z=0
 norm_vec = [-1 * ray[0], ray[1], 0]
 
-y_slice = yt.SlicePlot(ds, 'y', 'density', north_vector = [0, 0, 1])
-cut = yt.SlicePlot(ds, norm_vec, 'density', north_vector = [0, 0, 1])
+y_slice = yt.SlicePlot(ds, 'y', 'density', north_vector = [0, 0, 1], center=center)
+cut = yt.SlicePlot(ds, norm_vec, 'density', north_vector = [0, 0, 1], center=center)
 
 ray_start =ds.arr(ray_start, "code_length")
 ray_end = ds.arr(ray_end, "code_length")
