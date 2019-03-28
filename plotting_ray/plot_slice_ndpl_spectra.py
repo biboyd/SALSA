d5e8851074ff85ccf29b2b62d6ce022ffaeed11a
boydbre1 <boydbre1@msu.edu>
2019-03-28 00:59:38 -0400

    added all plotting functions to plot_slice_ndpl_spectra.py

--- plotting_ray/plot_slice_ndpl_spectra.py
+++ plotting_ray/plot_slice_ndpl_spectra.py
@@ -6,20 +6,17 @@ import h5py
 # =================== #
 #
 #
-#
+# =================== #
 
-def plot_slice(ds_file_name, ray_file_name, field='density'):
+def plot_slice(ds, ray, ray_h5file, field='density'):
     """
 
     """
-    ds = yt.load(ds_file_name)
-    ray = yt.load(ray_file_name)
 
     # get beginning and end of ray
-    f = h5py.File(ray_file_name)
-    x = f['grid']['x']
-    y = f['grid']['y']
-    z = f['grid']['z']
+    x = ray_h5file['grid']['x']
+    y = ray_h5file['grid']['y']
+    z = ray_h5file['grid']['z']
 
     ray_begin = np.array([ x[0], y[0], z[0] ])
     ray_end = np.array([ x[-1], y[-1], z[-1] ])
@@ -36,7 +33,7 @@ def plot_slice(ds_file_name, ray_file_name, field='density'):
     ray_center = (ray_begin + ray_end)/2
     ray_center[2] = center[2]
 
-    #Create slice along ray. keep image pointed in z-dir
+    #Create slice along ray. keep slice pointed in z-direction
     slice = SlicePlot(ds,
                       norm_vector,
                       field,
@@ -47,6 +44,88 @@ def plot_slice(ds_file_name, ray_file_name, field='density'):
     return slice
 
 
+def plot_spect(ray, out_fname):
+    """
+
+    """
+
+    #generate spectrum defined by inputs
+    spect_gen = ray.SpectrumGenerator('COS')
+    spect_gen.make_spectrum(tri_ray, lines=line_list)=
+
+    #plot spectrum
+    spect_gen.plot_spectrum(out_fname)
+
+    #check if out name is already in .png format
+    if out_fname[-4:] != ".png":
+        out_fname += ".png"
+
+    return out_fname
+
+def plot_num_density(ray_h5file, ion_name, out_fname):
+    """
+
+    """
+    #get list of num density and corresponding lengths
+    num_density = list(ray_h5file['grid'][ion_p_name(ion_name)+'_number_density'])
+    dl_list = list(ray_h5file['grid']['dl'])
+
+    #convert list of dl's to list of lengths from begin of ray
+    num_dls = len(dl_list)
+    for i in range(1, num_dls):
+        dl_list[i] += dl_list[i-1]
+
+    #make num density plots
+
+    fig = plt.figure()
+    plt.plot(dl_list, num_density)
+
+    plt.save(out_fname)
+
+    return fig
+
+
+
+def compute_col_density(ray_h5file, ion_name):
+    """
+
+    """
+    #get list of num density and corresponding length
+    num_density = np.array(ray_h5file['grid'][ion_p_name(ion_name)+'_number_density'])
+    dl_array = np.array(ray_h5file['grid']['dl'])
+
+    #multiply num density by its dl and sum up to get column density
+    col_density = np.sum( num_density*dl_array )
+
+    return col_density
+def ion_p_name(ion_name):
+    """
+    convert ion species name from trident style to one that
+    can be used with h5 files
+    """
+
+    # 'H I' is an exception just return H
+    if ion_name == 'H I':
+        return 'H'
+
+    #split up the words in ion species name
+    ion_split = ion_name.split()
+
+    #convert num from roman numeral. subtract run b/c h5
+    num = tri.from_roman(ion_name[1])-1
+
+    #combine all the names
+    outname = ion_split[0] + '_p' + num
+    return outname
+
+def open_files(ds_file_name, ray_file_name):
+    ds = yt.load(ds_file_name)
+    ray = yt.load(ray_file_name)
+    ray_h5file = h5py.File(ray_file_name)
+
+    return ds, ray, ray_h5file
+
 if __name__ == '__main__':
     data_set_fname = argv[1]
     ray_fname = argv[2]
+    ds, ray = open_files(data_set_fname, ray_fname)
