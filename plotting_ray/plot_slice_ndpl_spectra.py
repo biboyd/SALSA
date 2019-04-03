f0222a073bb3b90fd84f1a2c807e769a59827c78
boydbre1 <boydbre1@msu.edu>
2019-04-02 23:08:23 -0400

    debugging main plotting and edited playground notebook

--- plotting_ray/plot_slice_ndpl_spectra.py
+++ plotting_ray/plot_slice_ndpl_spectra.py
@@ -1,8 +1,11 @@
 import yt
-import trident
+import trident as tri
 import numpy as np
 from sys import argv
 import h5py
+from os import remove
+import matplotlib.pyplot as plt
+
 # =================== #
 #
 #
@@ -25,7 +28,7 @@ def plot_slice(ds, ray, ray_h5file, field='density'):
     ray_vec = ray_end - ray_begin
 
     #construct vec orthogonal to ray
-    norm_vector = [ray[1], -1*ray[0], 0]
+    norm_vector = [ray_vec[1], -1*ray_vec[0], 0]
 
     #get center of ray keep z at center
     center = ds.domain_center
@@ -34,35 +37,48 @@ def plot_slice(ds, ray, ray_h5file, field='density'):
     ray_center[2] = center[2]
 
     #Create slice along ray. keep slice pointed in z-direction
-    slice = SlicePlot(ds,
+    slice = yt.SlicePlot(ds,
                       norm_vector,
                       field,
                       north_vector = [0, 0, 1],
                       center = center)
-
+    # add ray to slice
     slice.annotate_ray(ray)
-    return slice
+
+    # create frb slice that can be plotted using imshow
+    width = (10, 'kpc')
+    res = [1000, 1000]
+    frb_slice = slice.to_frb(width, res, center = center)
+
+    return frb_slice
 
 
-def plot_spect(ray, out_fname):
+def plot_spect(ray, ion_name, fname=".temp.h5"):
     """
 
     """
-
+    line_list = [ion_name]
     #generate spectrum defined by inputs
-    spect_gen = ray.SpectrumGenerator('COS')
-    spect_gen.make_spectrum(tri_ray, lines=line_list)=
+    spect_gen = tri.SpectrumGenerator('COS')
+    spect_gen.make_spectrum(ray, lines=line_list, output_file = fname)
 
-    #plot spectrum
-    spect_gen.plot_spectrum(out_fname)
+    #save the spectrum to hdf5
+    spect_gen.save_spectrum(fname)
 
-    #check if out name is already in .png format
-    if out_fname[-4:] != ".png":
-        out_fname += ".png"
+    #open saved file to get wavelength and flux
+    spect_h5 = h5py.File(fname)
 
-    return out_fname
+    wavelength = np.array(spect_h5['wavelength'])
+    flux = np.array(spect_h5['flux'])
 
-def plot_num_density(ray_h5file, ion_name, out_fname):
+    #close and delete temp file
+    spect_h5.close()
+    remove(fname)
+
+    #plot values
+    plt.plot(wavelength, flux)
+
+def plot_num_density(ray_h5file, ion_name):
     """
 
     """
@@ -77,12 +93,7 @@ def plot_num_density(ray_h5file, ion_name, out_fname):
 
     #make num density plots
 
-    fig = plt.figure()
     plt.plot(dl_list, num_density)
-
-    plt.save(out_fname)
-
-    return fig
 
 
 
@@ -98,6 +109,7 @@ def compute_col_density(ray_h5file, ion_name):
     col_density = np.sum( num_density*dl_array )
 
     return col_density
+
 def ion_p_name(ion_name):
     """
     convert ion species name from trident style to one that
@@ -128,4 +140,10 @@ def open_files(ds_file_name, ray_file_name):
 if __name__ == '__main__':
     data_set_fname = argv[1]
     ray_fname = argv[2]
-    ds, ray = open_files(data_set_fname, ray_fname)
+    ds, ray, rayh5 = open_files(data_set_fname, ray_fname)
+    ion = 'H I'
+
+    plot_spect(ray, "spectra.txt")
+    frb = plot_slice(ds, ray, rayh5)
+    plot_num_density(rayh5, ion)
+    print(compute_col_density(rayh5, ion))
