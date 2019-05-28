4b794ad88c0fc2eb90bdcc1e0e7de3f335e8fa30
boydbre1 <boydbre1@msu.edu>
2019-05-28 11:12:25 -0400

    files for running ray construct and movie construct

---
+++ plotting_ray/test_ray_construct.py
@@ -0,0 +1,14 @@
+import plotter
+from sys import argv
+
+#setup conditions
+line_list = ['H I', 'O VI', 'C IV']
+
+filename = argv[1]
+
+n_rays=int(argv[2])
+out_dir = argv[3] 
+
+#now actual test
+plotter.construct_rays(filename, line_list, n_rays=n_rays, out_dir=out_dir)
+print("finished")
