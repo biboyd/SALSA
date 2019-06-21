eca10b8f49b491b2ff9b53dcefe098704e1b0e86
boydbre1 <boydbre1@msu.edu>
2019-06-21 14:17:07 -0400

    rays now positioned by normal vector to galaxy

--- plotting_ray/construct_rays.py
+++ plotting_ray/construct_rays.py
@@ -5,19 +5,19 @@ from mpi4py import MPI
 from sys import argv
 from os import makedirs
 import errno
+from scipy.spatial.transform import Rotation
 
 
 def construct_rays( dataset,
                     line_list,
                     length=200,
                     n_rays=100,
-                    direction='z',
+                    norm_vector=[0, 0, 1],
                     angle=0,
-                    dist_from_center=200,
+                    max_impact_param=200,
                     center=None,
                     parallel=False,
-                    out_dir='./rays',
-                    DEBUG=False):
+                    out_dir='./rays'):
     """
     Constructs a number of light rays to "scan" a galactic data set using trident
 
@@ -26,9 +26,9 @@ def construct_rays( dataset,
         line_list: list of ions to include in rays
         length: the length of the rays in kpc
         n_rays: the number of rays to construct
-        direction: the coordinate direction in which to "scan" the galaxy
+        norm_vector: normal vector of galaxy. (perpendiuclar to disk)
         angle: The azimuthal angle around the direction. in degrees
-        dist_from_center: range to construct rays. in kpc from center of galaxy
+        max_impact_param: sets range to construct rays. in kpc from center of galaxy
         center : array/list. Coordinates to center ray creation. Defaults to dataset/s center. in kpc
         parallel : boolean. If parallelization is being used set to true.
         out_dir: directory in which to save the rays
@@ -36,18 +36,14 @@ def construct_rays( dataset,
     Returns:
         none
     """
-    if DEBUG:
-        print(trident.__version__)
+
     ds = yt.load(dataset)
     #add ion fields to dataset if not already there
     trident.add_ion_fields(ds, ions=line_list, ftype='gas')
 
     #create directory if doesn't exist
-    try:
-        makedirs(out_dir)
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
+    makedirs(out_dir, exist_ok=True)
+
 
     #start MPI in case parallelization is being used
     if parallel:
@@ -55,75 +51,55 @@ def construct_rays( dataset,
 
     #convert lengths to code_length and angle to radians
     length = ds.quan(length, 'kpc').in_units('code_length')
-    dist_from_center = ds.quan(dist_from_center, 'kpc').in_units('code_length')
-    angle = np.deg2rad(angle)
+    max_impact_param = ds.quan(max_impact_param, 'kpc').in_units('code_length')
+    norm_vector = norm_vector/np.linalg.norm(norm_vector)
+
+    #create the unit vector for rays
+    ray_unit = np.array( [norm_vector[2], 0, -norm_vector[0]] )
+    ray_unit = ray_unit/np.linalg.norm(ray_unit)
+
+    #check not zero vector
+    if ray_unit[0] == 0 and ray_unit[2] == 0:
+        #switch to a non zero vector
+        ray_unit = np.array( [0, norm_vector[2], -norm_vector[1]] )
+
+    #rotate ray unit
+    if angle != 0:
+        angle = np.deg2rad(angle)
+        rot_vector = norm_vector*angle
+        rot = Rotation.from_rotvec(rot_vector)
+        ray_unit = rot.apply(ray_unit)
 
     if center is None:
         center = ds.domain_center.in_units('code_length')
     else:
-        center = ds.arr(center, 'kpc').in_units('code_length')
+        center = ds.arr(center, 'code_length')
 
-    #get right indexing for direction
-    if (direction == 'x'):
-        dir_index=0
-        coord1_index=1
-        coord2_index=2
-    elif (direction == 'y'):
-        dir_index=1
-        coord1_index=2
-        coord2_index=0
-    elif (direction == 'z'):
-        dir_index=2
-        coord1_index=0
-        coord2_index=1
-    else:
-        raise RuntimeError("direction must be 'x', 'y', or 'z'")
+    #find the beginning and ending centers of all rays
+    start_ray_cent = center + max_impact_param*norm_vector
+    end_ray_cent = center - max_impact_param*norm_vector
+    ray_centers = np.linspace(start_ray_cent, end_ray_cent, n_rays)
 
-    #calculate the changing coordinate
-    begin = center[dir_index] + dist_from_center
-    end = center[dir_index] - dist_from_center
-    direct_coord = np.linspace(begin, end, n_rays)
-
-    #compute ray length in the coordinate directions
-    len_coord1 = length/2* np.cos(angle)
-    len_coord2 = length/2* np.sin(angle)
-
-    #calc beginning and ending of ray for constant coordinates
-    coord1_begin = center[coord1_index] - len_coord1
-    coord1_end = center[coord1_index] + len_coord1
-
-    coord2_begin = center[coord2_index] - len_coord2
-    coord2_end = center[coord2_index] + len_coord2
-
-    #empty ray coordinates to be filled
-    ray_begin = ds.arr(np.zeros(3), "code_length")
-    ray_end = ds.arr(np.zeros(3), "code_length")
+    #add offset to find end and beginning to all rays
+    offset = length/2 * ray_unit
+    ray_begins = ray_centers - offset
+    ray_ends = ray_centers + offset
 
     #set padding for filenames
     pad = np.floor( np.log10(n_rays) )
     pad = int(pad) + 1
-
 
     my_ray_nums = np.arange(n_rays)
     if parallel:
         #split ray numbers then take a portion based on rank
         split_ray_nums = np.array_split(my_ray_nums, comm.size)
         my_ray_nums = split_ray_nums[ comm.rank ]
+
     for i in my_ray_nums:
-        #set beginning ray
-        ray_begin[dir_index] = direct_coord[i]
-        ray_begin[coord1_index] = coord1_begin
-        ray_begin[coord2_index] = coord2_begin
-
-        #set ending ray
-        ray_end[dir_index] = direct_coord[i]
-        ray_end[coord1_index] = coord1_end
-        ray_end[coord2_index] = coord2_end
-
         #construct ray
         trident.make_simple_ray(ds,
-                                ray_begin,
-                                ray_end,
+                                ray_begins[i],
+                                ray_ends[i],
                                 lines=line_list,
                                 data_filename= f"{out_dir}/ray{i:0{pad}d}.h5")
     if parallel:
@@ -135,10 +111,9 @@ def construct_rays( dataset,
 if __name__ == '__main__':
     #setup conditions
     line_list = ['H I', 'O VI', 'C IV']
-
     if len(argv) == 5:
         filename = argv[1]
-        n_rays=int(argv[2])
+        num_rays=int(argv[2])
         ray_length=int(argv[3])
         out_dir = argv[4]
     else:
@@ -147,9 +122,9 @@ if __name__ == '__main__':
 
     #divide rays evenly
     construct_rays(filename, line_list,
-                    n_rays=n_rays,
+                    n_rays=num_rays,
+                    norm_vector = [0.5, 0, 0.5],
                     length=ray_length,
-                    dist_from_center=ray_length/2,
+                    max_impact_param=ray_length/2,
                     out_dir=out_dir,
                     parallel = True)
-
