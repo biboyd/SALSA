7dced9f7027b4eea5b9d5d269c4a1b59be2b7967
boydbre1 <boydbre1@msu.edu>
2019-07-08 17:11:50 -0400

    rotate only going half way?

--- rotate_projection/rotate_projection.py
+++ rotate_projection/rotate_projection.py
@@ -1,7 +1,11 @@
+import sys
+sys.path.insert(0, '/mnt/home/boydbre1/Repo/CGM/cosmo_analysis/')
+from center_finder import find_center
 import yt
 import numpy as np
 from mpi4py import MPI
 from scipy.spatial.transform import Rotation
+from os import makedirs
 
 def create_proj_frames(ds_fname,
                        center,
@@ -17,17 +21,17 @@ def create_proj_frames(ds_fname,
 
     if comm.rank == 0:
         #get in plane of galaxy vector to rot offset
-        inplane_vec = np.array( [norm_vec[2], 0, -norm_vec[0]] )
+        inplane_vec = np.array( [normal_vec[2], 0, -normal_vec[0]] )
 
         #check not zero vector
         if inplane_vec[0] == 0 and inplane_vec[2] == 0:
             #switch to a non zero vector
-            inplane_vec = np.array( [0, norm_vec[2], -norm_vec[1]] )
+            inplane_vec = np.array( [0, normal_vec[2], -normal_vec[1]] )
 
         inplane_vec = inplane_vec/np.linalg.norm(inplane_vec)
         #create rotation vector then rotate norm vec to get our axis of rot
         rot_vec = inplane_vec * np.deg2rad(offset)
-        rot = Rotation.from_rotvec(rot_vector)
+        rot = Rotation.from_rotvec(rot_vec)
         axis_rot_vec = rot.apply(normal_vec)
         axis_rot_vec = np.float64(axis_rot_vec)
 
@@ -38,7 +42,7 @@ def create_proj_frames(ds_fname,
     comm.Barrier()
     comm.Bcast([axis_rot_vec, MPI.DOUBLE])
     comm.Bcast([inplane_vec, MPI.DOUBLE])
-    rotatations = np.linspace(0, 2*np.pi, num_frames)
+    rotations = np.linspace(0, 2*np.pi, num_frames)
     my_rot_nums = np.arange(num_frames)
     #split ray numbers then take a portion based on rank
     split_rot_nums = np.array_split(my_rot_nums, comm.size)
@@ -46,9 +50,9 @@ def create_proj_frames(ds_fname,
 
     #load ds and construct sphere around galaxy
     ds = yt.load(ds_fname)
-    sph = ds.sphere(center, (50, 'kpc'))
+    sph = ds.sphere(center, (100, 'kpc'))
 
-    pad = np.ceil( np.log10(num_frames) )
+    pad = int(np.ceil( np.log10(num_frames)))
     for i in my_rot_nums:
         #construct rotation vector and use to rotate
         rot_vec = axis_rot_vec * rotations[i]
@@ -56,14 +60,17 @@ def create_proj_frames(ds_fname,
         proj_vec = rot.apply(inplane_vec)
 
         prj = yt.OffAxisProjectionPlot(ds, proj_vec, field,
-                                       center=center, width=(75, 'kpc'),
+                                       center=center, width=(100, 'kpc'),
                                        north_vector=normal_vec,
                                        weight_field=weight,
                                        data_source=sph)
         prj.save(f"{out_dir}/proj{i:0{pad}d}.png")
 
 if __name__ == '__main__':
-    dsname = argv[1]
+    dsname = sys.argv[1]
+    frms = int(sys.argv[2]) 
+    out_dir = sys.argv[3]
 
     c, n, r, bv = find_center(dsname)
-    create_proj_frames('x', 1, 2)
+    makedirs(out_dir, exist_ok=True)
+    create_proj_frames(dsname, c, n, num_frames=frms, out_dir=out_dir)
