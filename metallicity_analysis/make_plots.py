009658e200ceedcb367bbd9c03029be5df5a922d
boydbre1 <boydbre1@msu.edu>
2020-01-10 11:15:40 -0500

    make title adaptable

--- metallicity_analysis/make_plots.py
+++ metallicity_analysis/make_plots.py
@@ -1,14 +1,17 @@
+import matplotlib as mpl
+mpl.use('Agg')
+
 import numpy as np
 import matplotlib.pyplot as plt
 import yt
 import trident
 import sys
 
-def main(ds_name, array_name, out_dir='./'):
+def main(ds_fname, array_name, impact_param='??', out_dir='./'):
     absorber_array=np.load(array_name)
     ds = yt.load(ds_fname)
 
-    fig1 = plt.figure(figsize=(8, 6))
+    fig = plt.figure(figsize=(12, 6))
     metallicity=[]
     logNH=[]
 
@@ -24,11 +27,14 @@ def main(ds_name, array_name, out_dir='./'):
     slls_metal=[]
     dla_metal=[]
 
+    n_absorbers=0
+    #seperate absorbers into appropriate lists
     for absorber in absorber_array:
         if absorber[3] > 15:
             curr_metal = ds.quan(absorber[7], 'dimensionless').in_units('Zsun')
             metallicity.append(np.log10(curr_metal))
             logNH.append(absorber[3])
+            n_absorbers+=1
 
             if absorber[3] < 16.2:
                 slfs_metal.append(np.log10(curr_metal))
@@ -48,45 +54,46 @@ def main(ds_name, array_name, out_dir='./'):
 
 
     #plot all the pretty colors
-    plt.scatter(slfs_logNH, slfs_metal, color='tab:grey', edgecolor='black', label='SLFSs')
-    plt.scatter(plls_logNH, plls_metal, color='tab:blue', edgecolor='black', label='pLLSs')
-    plt.scatter(lls_logNH, lls_metal, color='red', edgecolor='black', label='LLSs')
-    plt.scatter(slls_logNH, slls_metal, color='tab:brown', edgecolor='black', label='sLLSs')
-    plt.scatter(dla_logNH, dla_metal, color ='cyan', edgecolor='black', label='DLAs')
-    plt.legend()
+    ax_scat = fig.add_subplot(122)
+    ax_scat.scatter(slfs_logNH, slfs_metal, color='tab:grey', edgecolor='black', label='SLFSs')
+    ax_scat.scatter(plls_logNH, plls_metal, color='tab:blue', edgecolor='black', label='pLLSs')
+    ax_scat.scatter(lls_logNH, lls_metal, color='red', edgecolor='black', label='LLSs')
+    ax_scat.scatter(slls_logNH, slls_metal, color='tab:brown', edgecolor='black', label='sLLSs')
+    ax_scat.scatter(dla_logNH, dla_metal, color ='cyan', edgecolor='black', label='DLAs')
+    ax_scat.legend()
     #plt.plot(logNH, metallicity, ".")
-    plt.ylabel("$log$ $\\frac{Z}{Z_{\\odot}}$", size=15)
-    plt.xlabel("$log$ $N_{HI}$", size=15)
-    plt.xlim(15, 22)
-    plt.ylim(-3.4,0.6)
+    ax_scat.set_ylabel("$log$ $\\frac{Z}{Z_{\\odot}}$", size=15)
+    ax_scat.set_xlabel("$log$ $N_{HI}$", size=15)
+    ax_scat.set_xlim(15, 22)
+    ax_scat.set_ylim(-3.4,0.6)
 
-    plt.hlines([0.0, -1.4],15, 22,linestyle='dashed', alpha=0.5, zorder=1 )
-    plt.title("356 Absorbers. max_impact=100kpc. z=0.3")
+    ax_scat.hlines([0.0, -1.4],15, 22,linestyle='dashed', alpha=0.5, zorder=1 )
+    ax_scat.set_title(f"{n_absorbers} Absorbers. max_impact={impact_param}kpc. z={ds.current_redshift:0.2f}")
 
     #now make histogram
-    fig2 = plt.figure(figsize=(8, 6))
+    ax_hist = fig.add_subplot(121)
     binwidth=0.2
     binrange=[-5, 2]
     bins = (binrange[1]-binrange[0])/binwidth
     bins = int(bins)
-    plt.hist(slfs_metal, bins=bins,
+    ax_hist.hist(slfs_metal, bins=bins,
              range=binrange,
              density=True,
              label=f"{len(slfs_metal)} SLFSs")
-    plt.hist(plls_metal, bins=bins,
+    ax_hist.hist(plls_metal, bins=bins,
              range=binrange,
              density=True,
              linewidth=2,
              histtype='step',
              label=f"{len(plls_metal)} pLLSs")
 
-    plt.legend()
-    plt.title("Normalized Histogram of SLFSs and pLLSs")
-    plt.xlabel("$log$ $\\frac{Z}{Z_{\\odot}}$", size=15)
-    plt.ylabel("Normalized Count")
+    ax_hist.legend()
+    ax_hist.set_title("Normalized Histogram of SLFSs and pLLSs")
+    ax_hist.set_xlabel("$log$ $\\frac{Z}{Z_{\\odot}}$", size=15)
+    ax_hist.set_ylabel("Normalized Count")
 
-    fig1.save(f"{out_dir}/metallicity_H_col_density.png")
-    fig2.save(f"{out_dir}/histo_SLFS_pLLS.png")
+    fig.tight_layout()
+    fig.savefig(f"{out_dir}/metallicity_H_col_density.png")
 
 if __name__ == '__main__':
     ds_name = sys.argv[1] #"RD0036"
@@ -97,8 +104,8 @@ if __name__ == '__main__':
         out_dir='./'
     frac=0.8
 
-    ds_fname = f"/mnt/gs18/scratch/users/boydbre1/cosmological/foggie/{dsname}/{dsname}"
-    arr_dir=f"/mnt/gs18/scratch/users/boydbre1/metallicity_analysis/foggie/{dsname}/max_impact{max_b}/output{frac}"
+    ds_fname = f"/mnt/gs18/scratch/users/boydbre1/cosmological/foggie/{ds_name}/{ds_name}"
+    arr_dir=f"/mnt/gs18/scratch/users/boydbre1/metallicity_analysis/foggie/{ds_name}/max_impact{max_b}/output{frac}"
     arr_name = f"{arr_dir}/all_absorbers.npy"
 
-    main(ds_name, arr_name, out_dir=out_dir)
+    main(ds_fname, arr_name, impact_param=max_b, out_dir=out_dir)
