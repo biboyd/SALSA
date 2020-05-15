42dcf5b1e5b34f49d4631e23829c9cca582d3939
boydbre1 <boydbre1@msu.edu>
2020-05-15 17:26:30 -0400

    remove unused scripts

--- prev_analysis/tunedCGM_analysis/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import yt
-import trident
-yt.enable_parallelism()
-
-print('test')
-
-ds = yt.load('/mnt/scratch/dsilvia/simulations/reu_sims/MW_1638kpcBox_800pcCGM_200pcDisk_lowres/DD1000/DD1000')
