817253f5a796f2eab013ce40239643ad307c7fbf
biboyd <brendanboyd16@gmail.com>
2020-06-12 17:55:51 -0400

    Add init file to access things more easily

---
+++ SALS/__init__.py
@@ -0,0 +1,18 @@
+"""
+SALS
+Written by: Brendan Boyd brendan.boyd@stonybrook.edu
+
+"""
+
+__version__ = "0.0.0"
+
+from absorber_extractor import absorber_extractor
+
+from absorber_plotter import absorber_plotter
+
+from construct_random_rays import random_rays
+
+from collect_features import create_frames
+
+
+from utils.filter_definitions import parse_cut_filter
