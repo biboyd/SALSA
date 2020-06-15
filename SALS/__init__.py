8d9fac647afc1b1b7a29c7316781fed33ee49cc0
biboyd <brendanboyd16@gmail.com>
2020-06-15 13:09:34 -0400

    add doc strings. fix imports

--- SALS/__init__.py
+++ SALS/__init__.py
@@ -6,13 +6,13 @@ Written by: Brendan Boyd brendan.boyd@stonybrook.edu
 
 __version__ = "0.0.0"
 
-from absorber_extractor import absorber_extractor
+from SALS.absorber_extractor import absorber_extractor
 
-from absorber_plotter import absorber_plotter
+from SALS.absorber_plotter import absorber_plotter
 
-from construct_random_rays import random_rays
+from SALS.construct_random_rays import random_rays
 
-from collect_features import create_frames
+from SALS.collect_features import create_frames
 
 
-from utils.filter_definitions import parse_cut_filter
+from SALS.utils.filter_definitions import parse_cut_filter
--- SALS/utils/collect_files.py
+++ SALS/utils/collect_files.py
@@ -8,6 +8,29 @@ def collect_files(directory, file_ext='.h5', key_words=[], black_list=[]):
     """
     Finds and returns files in a given directory with given extension. Optional
     key_word and black_list requirements
+
+    Parameters
+    ----------
+
+    : directory : str
+        Directory in which to search for files
+
+    : file_ext : str
+        The file extension to look for (ie '.py', '.h5')
+
+    : key_words : list of str
+        Key words that must in the filename to be collected
+
+    : black_list : list of str
+        list of files to exclude from collection
+
+    Returns
+    -------
+
+    : files : list of str
+        List of filenames in the directory that pass all requirements for file
+        extension, keywords, black_list.
+
     """
 
     all_files = listdir(directory)
@@ -25,12 +48,18 @@ def check_file(file, key_words, black_list):
     Check the file against a black list as well as check if it has keywords in
     it.
 
-    Parameters:
-        file : string : filename to check
-        key_words: list : list of strings required to be in file name
-        black_list :list : list of files that are black listed, that file can't be called
-    Returns:
-        bool : Returns True if file is not in black_list and contains all key_words
+    Parameters
+    ----------
+    : file : string
+        filename to check
+    : key_words : list
+        list of strings required to be in file name
+    : black_list : list
+        list of files that are black listed, that file can't be called
+    Returns
+    --------
+    : : bool
+        Returns True if file is not in black_list and contains all key_words
     """
     #check if ile in black list
     if file in black_list:
