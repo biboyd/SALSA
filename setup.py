0b137e7b48e8228e8b45d3816f874794354ac168
biboyd <brendanboyd16@gmail.com>
2020-06-17 15:15:41 -0400

    dont load mpi4py if building on rtd

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
+import os
+
+#base dependencies
+dependecies=['numpy', 'yt', 'trident', 'spectacle', 'matplotlib', 'pandas', 'scipy']
+
+#if not building on rtd then add mpi4py
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if not on_rtd:
+    dependecies += 'mpi4py'
+
 setup(name="salsa",
       version="0.0.0",
       author="Brendan Boyd",
       packages=find_packages(),
-      install_requires=[
-        'numpy',
-        'yt',
-        'trident',
-        'spectacle',
-        'matplotlib',
-		'pandas',
-        'mpi4py',
-        'scipy'
-      ])
+      install_requires=dependecies)
